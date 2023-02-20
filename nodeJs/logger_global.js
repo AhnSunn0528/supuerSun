7bffbd188511274ebdf9c84c0b59081c9f351158
sunyoung <sunyoah@naver.com>
2023-02-20 16:38:20 +0900

    test

---
+++ nodeJs/logger_global.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+console.log('hello %s', world);
+
+const world = 'world';
+console.log(`hellpo ${world}`);
+console.error(new Error('에러 메세지 출력'));
+
+const arr = [
+    { name: 'John Doe', email: 'john@email.com' },
+    { name: 'Jeremy Go', email: 'jeremy@email.com' }
+]
